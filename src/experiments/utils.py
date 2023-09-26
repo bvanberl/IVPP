Fixed weighted Barlow variance calculation

src/experiments/utils.py
@@ -170,15 +170,15 @@ def get_classification_metrics(
     metrics["auc"] = roc_auc_score(y_true, y_prob)
     metrics["accuracy"] = accuracy_score(y_true, y_pred)
     if n_classes == 2:
-        metrics["precision"] = precision_score(y_true, y_pred)
-        metrics["recall"] = recall_score(y_true, y_pred)
-        metrics["f1"] = f1_score(y_true, y_pred)
+        metrics["precision"] = precision_score(y_true, y_pred, zero_division=0.)
+        metrics["recall"] = recall_score(y_true, y_pred, zero_division=0.)
+        metrics["f1"] = f1_score(y_true, y_pred, zero_division=0.)
     else:
         for i in range(n_classes):
             y_true_class = (y_true == i).astype(np.int64)
             y_pred_class = (y_pred == i).astype(np.int64)
-            metrics["class{i}_precision"] = precision_score(y_true_class, y_pred_class)
-            metrics["class{i}_recall"] = recall_score(y_true_class, y_pred_class)
-            metrics["class{i}_f1"] = f1_score(y_true_class, y_pred_class)
+            metrics["class{i}_precision"] = precision_score(y_true_class, y_pred_class, zero_division=0.)
+            metrics["class{i}_recall"] = recall_score(y_true_class, y_pred_class, zero_division=0.)
+            metrics["class{i}_f1"] = f1_score(y_true_class, y_pred_class, zero_division=0.)
     return metrics
 

src/losses/utils.py
@@ -57,14 +57,17 @@ def weighted_moments(
     """Compute weighted moments along axis
 
     Determines the weighted mean and variance along the
-    desired axis
+    desired axis. Implemented according to definition at
+    https://www.itl.nist.gov/div898/software/dataplot/refman2/ch2/weighvar.pdf
     :param X: Matrix, with shape (h, w)
     :param dim: Samples axis
     :param weights: Weights per sample, with shape (`X.shape[dim]`)
     :return: weighted mean, weighted variance
     """
 
-    assert X.shape[dim] == weights.shape[0]
+    N = X.shape[dim]    # Number of data points
+    assert N == weights.shape[0]
+    N_prime = torch.count_nonzero(weights)  # Number of nonzero weights
     weights = torch.unsqueeze(weights, dim=1)
 
     # Calculate the weighted mean
@@ -75,6 +78,8 @@ def weighted_moments(
     squared_diff = torch.square(X - weighted_mean)
 
     # Calculate the weighted variance
-    weighted_var = torch.sum(squared_diff * weights) / weight_sum
+    numerator = torch.sum(squared_diff * weights, dim=dim)
+    denominator = (N_prime - 1.) * weight_sum / N_prime
+    weighted_var = numerator / denominator
 
     return weighted_mean, weighted_var

src/models/backbones.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Module, Sequential, Identity
+from torch.nn import Module, Sequential, Identity, AdaptiveAvgPool2d
 from torchvision.models import resnet18, efficientnet_v2_s, \
     mobilenet_v3_small, vgg16
 
@@ -85,7 +85,8 @@ def get_vgg16(
     if imagenet_weights:
         state_dict = torch.hub.load_state_dict_from_url(VGG16_WEIGHTS)
         backbone.load_state_dict(state_dict)
-    backbone.fc = Identity()
+    backbone.avgpool = AdaptiveAvgPool2d(output_size=(1,1))
+    backbone.classifier = Identity()
     if n_cutoff_layers > 0:
         backbone = Sequential(*list(backbone.children())[:-(n_cutoff_layers + 1)])
     return backbone

src/pretrain.py
@@ -1,15 +1,7 @@
-import os
 import argparse
 import datetime
-import json
 
-import tqdm
 import yaml
-import wandb
-import numpy as np
-import torch
-from torch.utils.tensorboard import SummaryWriter
-import torchsummary
 
 from src.models.joint_embedding import JointEmbeddingModel
 from src.losses.losses import *
@@ -60,7 +52,7 @@ if __name__ == '__main__':
     use_unlabelled = cfg['PRETRAIN']['USE_UNLABELLED']
     use_imagenet = cfg['PRETRAIN']['IMAGENET_WEIGHTS']
     augment_pipeline = cfg['PRETRAIN']['AUGMENT_PIPELINE']
-    channels = 3 if use_imagenet else 1
+    channels = 3 # if use_imagenet else 1
     us_mode = "bmode"   # TODO: Add M-mode
 
     train_ds, train_df, val_ds, val_set = load_data_for_pretrain(
@@ -106,8 +98,10 @@ if __name__ == '__main__':
             id=resume_id
         )
         print(f"Run config: {wandb_run}")
+        model_artifact = wandb.Artifact(f"pretrained:{method}", type="model")
     else:
         wandb_run = None
+        model_artifact = None
 
 
     # Define the base loss and initialize any regularizers
@@ -132,6 +126,8 @@ if __name__ == '__main__':
     # model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
     # model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
     #torchsummary.summary(model, input_size=(channels, width, height))
+    print(model.backbone)
+    print(model.projector)
 
     epochs = cfg['PRETRAIN']['EPOCHS']
     base_lr = cfg['PRETRAIN']['INIT_LR']
@@ -165,6 +161,7 @@ if __name__ == '__main__':
     scaler = torch.cuda.amp.GradScaler()
     log_interval = args["log_interval"]
     pretrain_state = {}
+
 
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1}/{epochs} " + "=" * 30 + "\n")
@@ -230,15 +227,16 @@ if __name__ == '__main__':
             optimizer=optimizer.state_dict(),
             pretrain_method=method
         )
-        torch.save(pretrain_state, os.path.join(checkpoint_dir, "checkpoint.pth"))
+        checkpoint_path = os.path.join(checkpoint_dir, "checkpoint.pth")
+        torch.save(pretrain_state, checkpoint_path)
+        if use_wandb:
+            model_artifact.add_file(checkpoint_path)
 
     # Save the final pretrained model
-    if use_wandb:
-        model_path = os.path.join(wandb.run.dir, f'{method}_pretrained')
-    else:
-        cur_datetime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        model_path = os.path.join(checkpoint_dir, "final_model.pth")
+    model_path = os.path.join(checkpoint_dir, "final_model.pth")
     torch.save(pretrain_state, model_path)
+    if use_wandb:
+        model_artifact.add_file(model_path)
 
     if use_wandb:
         wandb.finish()

src/train.py
@@ -1,16 +1,10 @@
-import os
 import argparse
 import datetime
 
 import json
-import tqdm
 import yaml
-import wandb
-import numpy as np
-import torch
-from torch.utils.tensorboard import SummaryWriter
 from torch.optim import SGD
-from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss
 import torchsummary
 
@@ -225,6 +219,8 @@ if __name__ == '__main__':
     h_dim = backbone(torch.randn((1,) + img_dim)).shape[-1]
     head = get_classifier_head(h_dim, fc_nodes, n_classes)
     classifier = Sequential(backbone, head).cuda(gpu)
+    print(backbone)
+    print(head)
 
     # Define an optimizer that assigns different learning rates to the
     # backbone and head.
@@ -233,11 +229,13 @@ if __name__ == '__main__':
     lr_backbone = run_cfg['lr_backbone']
     lr_head = run_cfg['lr_head']
     weight_decay = run_cfg['weight_decay']
+    momentum = run_cfg['momentum']
     param_groups = [dict(params=head.parameters(), lr=lr_head)]
     if experiment_type == "fine-tune":
         param_groups.append(dict(params=backbone.parameters(), lr=lr_backbone))
-    optimizer = SGD(param_groups, 0, momentum=0.9, weight_decay=weight_decay)
-    scheduler = CosineAnnealingLR(optimizer, epochs)
+    optimizer = SGD(param_groups, 0, weight_decay=weight_decay)
+    scheduler = ReduceLROnPlateau(optimizer, 'min')
+
     loss_fn = BCEWithLogitsLoss() if n_classes == 2 else CrossEntropyLoss()
     scaler = torch.cuda.amp.GradScaler()
     log_interval = args["log_interval"]
@@ -263,11 +261,10 @@ if __name__ == '__main__':
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
-            scheduler.step()   # Update LR
 
             # Log the loss and pertinent metrics.
             if global_step % log_interval == 0:
-                scalars = {"loss": loss, "lr": scheduler.get_last_lr()[-1], "epoch": epoch}
+                scalars = {"loss": loss, "lr_head": optimizer.param_groups[0]['lr'], "epoch": epoch}
                 if n_classes == 2:
                     y_pred = torch.greater_equal(y_prob, 0.5).to(torch.int64)
                 else:
@@ -298,16 +295,18 @@ if __name__ == '__main__':
         print(f"Epoch {epoch + 1}: " +
               ", ".join([f"val/{m}: {val_metrics[m]:.4f}" for m in val_metrics]))
 
-        # Save checkpoint if validation loss has improved
         val_loss = val_metrics["loss"]
+        scheduler.step(val_loss)  # Update LR if necessary
+
+        # Save checkpoint if validation loss has improved
         if val_loss < best_val_loss:
             print(f"Val loss improved from {best_val_loss} to {val_loss}. "
                   f"Updating checkpoint.")
-            best_val_loss  = val_loss
+            best_val_loss = val_loss
             torch.save(classifier.state_dict(), checkpoint_path)
 
     # Evaluate checkpoint with lowest validation loss on the test set
-    if args["test_eval"]:
+    if args["test_eval"] in ['Yes', 'yes', 'y', 'Y']:
         classifier.load_state_dict(torch.load(checkpoint_path))
         test_metrics = evaluate_on_dataset(
             test_ds,
@@ -321,7 +320,10 @@ if __name__ == '__main__':
             use_wandb
         )
         print(f"Test metrics:")
-        print(json.dumps(test_metrics, indent=4))
+        print(json.dumps(
+            {m: test_metrics[m].numpy() for m in test_metrics},
+            indent=4)
+        )
 
     if use_wandb:
         wandb.finish()

src/visualization/positive_pairs.py
@@ -35,7 +35,7 @@ def visualize_views(
     :param sample_weight: Display sample weight for each positive pair
     '''
 
-    n_channels = views[0].shape[-1]
+    n_channels = views[0].shape[1]
     colour_map = 'gray' if n_channels == 1 else 'viridis'
     num_views = len(views) - 1
     num_imgs = num_imgs if num_imgs else len(views[0])
@@ -44,6 +44,10 @@ def visualize_views(
     num_row = num_row + 1 if num_imgs % num_col else num_row
     mean_pixel_value = np.array(mean_pixel_value).reshape((1, 1, n_channels))
     std_pixel_value = np.array(std_pixel_value).reshape((1, 1, n_channels))
+
+    views[0] = np.moveaxis(views[0].numpy(), 1, -1)  # Channels-first to channels-last
+    views[1] = np.moveaxis(views[1].numpy(), 1, -1)
+    views[2] = views[2].numpy()
 
     # Plot the images
     fig, axes = plt.subplots(num_row, num_col, figsize=fig_size)
@@ -57,16 +61,12 @@ def visualize_views(
 
         scale = max_pixel_value * std_pixel_value
         offset = max_pixel_value * mean_pixel_value
-        views[0] = np.moveaxis(views[0].numpy(), 1, -1) # Channels-first to channels-last
-        views[1] = np.moveaxis(views[1].numpy(), 1, -1)
-        views[2] = views[2].numpy()
         pair = [((views[j][i] * scale) + offset).astype(np.uint8)
                 for j in range(num_views)]
-        divider = np.ones(
-            (views[0][i].shape[0], 3,
-             views[0][i].shape[-1])
-        ) * max_pixel_value
-        ax.imshow(np.concat([pair[0], divider, pair[1]], axis=1), cmap=colour_map)
+        divider = (np.ones(
+            (views[0][i].shape[0], 3, views[0][i].shape[-1])
+        ) * max_pixel_value).astype(np.uint8)
+        ax.imshow(np.concatenate([pair[0], divider, pair[1]], axis=1), cmap=colour_map)
         if len(views) > 2 and sample_weight:
             ax.set_title(f"sw: {views[2][:num_views]}")
         ax.set_axis_off()
@@ -88,7 +88,7 @@ def visualize_joint_views_dataset(
         num_imgs: int = 16,
         views_per_col: int = 8,
         fig_size: Tuple[int, int] = (24, 4),
-        max_pixel_value: float = 1.0,
+        max_pixel_value: float = 255.0,
         mean_pixel_value: Tuple[float, float, float] = (0., 0., 0.),
         std_pixel_value: Tuple[float, float, float] = (1., 1., 1.)
 ):